ca95f1fb6564b26a819d65f56dc9572b5747a956
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-07-13 14:21:36 -0300

    paso dos tests

--- dojo_6/javascript/test/solution.js
+++ dojo_6/javascript/test/solution.js
@@ -1,8 +1,26 @@
 const chai = require('chai');
+const Item = require('../item');
+const SellingPrice = require('../sellingPrice');
+const Cost = require('../cost');
+const Selling = require('../selling');
+const Profit = require('../profit');
+
 
 describe("checkout", () => {
-    describe("dummy", function() {
+    describe("dummy", function () {
         it("dummy", () => {
+            var item = new Item(new Cost(100));
+            var selling = new Selling([item] , new SellingPrice(150))
+            chai.assert.isTrue(selling.calculateProfit().equals(new Profit(50)));
+        });
+    });
+
+    describe("buy one and sell two", function () {
+        it("buy one and sell two", () => {
+            var item = new Item(new Cost(100));
+            var selling = new Selling([item,item],  new SellingPrice(250));
+
+            chai.assert.isTrue(selling.calculateProfit().equals(new Profit(50)));
         });
     });
 });
